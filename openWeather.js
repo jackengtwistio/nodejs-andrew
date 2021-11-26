c779f10b0211a2c970ba36c3481433cc8a65d48d
jackengtwistio <jtwist108@gmail.com>
2021-11-26 16:11:30 +0800

    added some error handling in openWeather

--- openWeather.js
+++ openWeather.js
@@ -8,9 +8,9 @@ function geoCode(address, callback) {
     const mapboxURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+address+".json?access_token=pk.eyJ1IjoiamFja2VuZ3R3aXN0aW8iLCJhIjoiY2t3ZnEyYTk0MGd1YjJubXBqNjRsNWRpOCJ9.xUf2stJGj3YQ9tlV5tM_Xw&limit=1"
     request({url: mapboxURL,json:true},(error,response) => {
         if (error) {
-            callback(error,undefined)
+            callback('can not request mapbox, maybe you are not connected to an internet ',undefined)
         }else if (response.body.features.length === 0){
-            console.log(`unable to find the address ${address}`)
+            console.log(`mapbox is unable to find the address ${address}`)
         }else{
             callback(undefined, {
                 coordinates: response.body.features[0].center,
@@ -23,17 +23,25 @@ function geoCode(address, callback) {
 }
 
 async function getWeather (lat,lon){
-    const openWeatherByCoordinatesURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5b52f0c85b3b5e27028cc42c5b6788eb`
+    const WeatherByCoordinatesURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5b52f0c85b3b5e27028cc42c5b6788eb`
+
+    
         
-    request({url: openWeatherByCoordinatesURL, json: true},(e,r,b)=>{
-        console.log(b)
+    request({url: WeatherByCoordinatesURL, json: true},(e,r,b)=>{
+        if (e) {
+            callback('can not request open weather, maybe your internet is slow ',undefined)
+        }else if (r.error){
+            // console.log(`mapbox is unable to find the address ${address}`)
+            console.log(`open weather is not working correctly,error: `+r.error)
+        }else if (b){console.log(b)}
+        else{console.log(`unknown error`)}
     })
 }
 
 geoCode(location,(error,response)=>{
-    const lat=response.lat
-    const lon = response.lon
-    // console.log(`error: ${error}`)
+    const { lat, lon } = response
+    if(error){console.log(`error: ${error}`)}
+
     // console.log(`response: ${response.cityName} ${response.lat} ${response.lon}`)
     if(response){
         getWeather(lat,lon)
