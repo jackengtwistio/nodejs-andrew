fb215f081fef2fc365ece707cdb98625fc0651f3
jackengtwistio <jtwist108@gmail.com>
2021-11-26 18:43:29 +0800

    apikey

--- openWeather.js
+++ openWeather.js
@@ -1,11 +1,10 @@
 const axios = require('axios')
 const request = require('postman-request')
-const location = process.argv.slice(2).join(' ')
-const APIkey = `5b52f0c85b3b5e27028cc42c5b6788eb`
-
+const location = process.argv.slice(2).join(' ')||`tibet`
+const {openWeatherKey, mapBoxKey} = require('./apikey/openWeatherApiKey')
 
 function geoCode(address, callback) {
-    const mapboxURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+address+".json?access_token=pk.eyJ1IjoiamFja2VuZ3R3aXN0aW8iLCJhIjoiY2t3ZnEyYTk0MGd1YjJubXBqNjRsNWRpOCJ9.xUf2stJGj3YQ9tlV5tM_Xw&limit=1"
+    const mapboxURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+address+".json?access_token="+mapBoxKey+"&limit=1"
     request({url: mapboxURL,json:true},(error,response) => {
         if (error) {
             callback('can not request mapbox, maybe you are not connected to an internet ',undefined)
@@ -23,29 +22,28 @@ function geoCode(address, callback) {
 }
 
 async function getWeather (lat,lon){
-    const WeatherByCoordinatesURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5b52f0c85b3b5e27028cc42c5b6788eb`
+    const WeatherByCoordinatesURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${openWeatherKey}`
 
     
         
     request({url: WeatherByCoordinatesURL, json: true},(e,r,b)=>{
         if (e) {
             callback('can not request open weather, maybe your internet is slow ',undefined)
-        }else if (r.error){
-            // console.log(`mapbox is unable to find the address ${address}`)
-            console.log(`open weather is not working correctly,error: `+r.error)
-        }else if (b){console.log(b)}
-        else{console.log(`unknown error`)}
+        }else if (b.cod === '400'){
+            console.log(b)
+            console.log(`error occured, maybe due to it can not find the location`)
+        }else if(b){
+            console.log(b)
+        }else{
+            console.log(`unknown error`)
+        }
     })
 }
 
 geoCode(location,(error,response)=>{
     const { lat, lon } = response
-    if(error){console.log(`error: ${error}`)}
-
-    // console.log(`response: ${response.cityName} ${response.lat} ${response.lon}`)
-    if(response){
-        getWeather(lat,lon)
-    }
+    if(error){ return console.log(`error: ${error}`)}
+    getWeather(lat,lon)
 })
 
 
