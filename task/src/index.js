f43b3b2d502de561dcee404af8cb3a4ef6437f29
jackengtwistio <jtwist108@gmail.com>
2021-12-01 16:23:11 +0800

    function findAndDo, pass variable as object key

--- task/src/index.js
+++ task/src/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const User = require('./models/user')
 const Task = require('./models/task');
-const { response } = require('express');
 require('./db/mongoose')
 
 const app = express()
@@ -14,13 +13,24 @@ const allTasks = Task.find({}).then(tasks => tasks)
 User.allowedProps = Object.keys(User.schema.tree)
 Task.allowedProps = Object.keys(Task.schema.tree)
 
+async function findAndDo(req,res,method,queryString,model,successMessage='succeeded') {
+    const prop = req.params[queryString]
+    const result = await model[method]({[queryString]:prop})
+    try{
+        if(!result||result.length===0){return res.send(`can't find ${prop}`)}
+        res.send(successMessage+': '+result)
+    }catch(err){
+        res.status(500).send(err)
+    }
+}
+
 async function handlePatch(req,res,queryString,model){
     const prop = req.params[queryString]
     const updates = req.body
     const updatesKeys = Object.keys(updates)
     async function response(){
         const result = await model.findOneAndUpdate(
-            {prop},
+            {[queryString]:prop},
             {...updates},
             {new: true, runValidators: true}
             )
@@ -40,6 +50,7 @@ app.get('/', async (req, res) => {
     const result = await allUsers+ await allTasks
     res.send(result)
 })
+
 app.post('/new/user', async (req, res) =>{ 
     const user = new User(req.body)
     console.log(user)
@@ -50,25 +61,10 @@ app.post('/new/user', async (req, res) =>{
         res.status(500).send(err)
     }
 })
-app.get('/users', async (req, res) =>{
-    res.status(200).send(await allUsers)
-})
-app.get('/users/:name', async (req, res) =>{
-    const name = req.params.name
-    console.log(name)
-    try{
-        if(!name){
-            const user = await User.find({})
-            res.send(user)
-        }else{ 
-            const user = await User.find({name})
-            res.send(user)
-        }
-    }catch(err){
-        res.status(500).send(err)
-    }
-})
+app.get('/users', async (req, res) =>res.status(200).send(await allUsers))
+app.get('/users/:name', async (req, res) =>findAndDo(req,res,'find','name',User))
 app.patch('/users/:name',(req, res) => handlePatch(req, res,'name',User))
+app.delete('/users/:name', async (req, res) =>findAndDo(req,res,'findOneAndDelete','name',User,'delete'))
 
 // -----------------------------------------------------------------
  
@@ -86,21 +82,9 @@ app.post('/new/task', async (req, res) =>{
 app.get('/tasks', async (req, res) =>{
     res.status(200).send(await allTasks)
 })
-app.get('/tasks/:title', async (req, res) =>{
-    const title = req.params.title
-    console.log(title)
-    try{
-        if(!title){ 
-            const task = await allTasks
-            res.send(task)
-        }else{ 
-            const task = await Task.find({title})
-            res.send(task)
-        }
-    }catch(err){
-        res.status(500).send(err)
-    }
-})
+app.get('/tasks/:title', (req, res) =>findAndDo(req,res,'find','title',Task))
 app.patch('/tasks/:title', (req, res) =>handlePatch(req, res, 'title', Task))
+app.delete('/tasks/:title', (req,res) =>findAndDo(req, res, 'findOneAndDelete', 'title', Task,'deleted'))
+
 
 app.listen(port,console.log(`app listening on ${port}`))
